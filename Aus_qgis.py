20174dcd3e6ba0ac9619d9f9be17f09bb0ea18cd
Or Caspi <orcaspi@gmail.com>
2021-10-25 10:23:10 -0600

    Update Aus_qgis.py

--- Aus_qgis.py
+++ Aus_qgis.py
@@ -1,20 +1,8 @@
-# Geocode dockless trips
-
-from qgis.core import *
-
-uri = "file:///C:/Users/orcas/Documents/GitHub/dissertation/dockless.csv?encoding=%s&delimiter=%s&xField=%s&yField=%s&crs=%s" % ("UTF-8",",", "Start.Longitude", "Start.Latitude","epsg:4269")
-starts = QgsVectorLayer(uri,"Starts","delimitedtext")
-QgsProject.instance().addMapLayer(starts)
-
-uri = "file:///C:/Users/orcas/Documents/GitHub/dissertation/dockless.csv?encoding=%s&delimiter=%s&xField=%s&yField=%s&crs=%s" % ("UTF-8",",", "End.Longitude", "End.Latitude","epsg:4269")
-ends = QgsVectorLayer(uri,"Ends","delimitedtext")
-QgsProject.instance().addMapLayer(ends)
-
-
 # Creating grid over Austin (0.001x0.001 deg)
 
 os.chdir("C:/Users/orcas/Documents/GitHub/dissertation/shapefiles")
 
+from qgis.core import *
 import processing
 
 grid = processing.run("native:creategrid", {'TYPE': '2',
@@ -26,14 +14,11 @@ grid = processing.run("native:creategrid", {'TYPE': '2',
 'CRS': 'EPSG:4269',
 'OUTPUT': 'grid.shp'})
 
-selected_layer = iface.addVectorLayer("grid.shp", '', 'ogr')
-
 
 # Refining grid by Austin's Boundaries
 
 boundary_path = "https://opendata.arcgis.com/datasets/09cd5b6811c54857bd3856b5549e34f0_0.geojson"
 boundaries = QgsVectorLayer(boundary_path, "boundaries", "ogr")
-QgsProject.instance().addMapLayer(boundaries)
 
 boundaries.selectByExpression(" \"CITY_NM\" = 'Austin' ")
 
@@ -42,3 +27,24 @@ selected_layer = iface.addVectorLayer("austin.shp", '', 'ogr')
 del(_writer)
 
 processing.run("qgis:clip", {'INPUT':"grid.shp", 'OVERLAY':"austin.shp", 'OUTPUT':"austin_grid.shp"})
+
+
+# Change coordinate system to 'State Plane Texas Central'
+
+austin_grid = QgsVectorLayer("austin_grid.shp", "austin", "ogr")
+processing.run('native:reprojectlayer', {'INPUT':austin, 'TARGET_CRS':'EPSG:32139', 'OUTPUT':"austin_grid_sp.shp"})
+
+
+# Geocode dockless trips
+
+uri = "file:///C:/Users/orcas/Documents/GitHub/dissertation/dockless.csv?encoding=%s&delimiter=%s&xField=%s&yField=%s&crs=%s" % ("UTF-8",",", "Start.Longitude", "Start.Latitude","epsg:4269")
+starts = QgsVectorLayer(uri,"Starts","delimitedtext")
+
+uri = "file:///C:/Users/orcas/Documents/GitHub/dissertation/dockless.csv?encoding=%s&delimiter=%s&xField=%s&yField=%s&crs=%s" % ("UTF-8",",", "End.Longitude", "End.Latitude","epsg:4269")
+ends = QgsVectorLayer(uri,"Ends","delimitedtext")
+#QgsProject.instance().addMapLayer(ends)
+
+
+# Aggregating trips
+
+
